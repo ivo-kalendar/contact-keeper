715b962fb1a142804588253d002483625932fc3b
Ivo <ivokalendar@icloud.com>
2020-10-02 09:39:24 +0200

    Backend completed.

--- routes/contacts.js
+++ routes/contacts.js
@@ -115,13 +115,32 @@ router.put('/:id', auth,
 //  @route    DELETE api/contact/:id
 //  @desc     Delete contact
 //  @access   Private
-router.delete('/:id', (req,res) => {
-	res.json({
-		file: "contacts.js",
-		routes: 4,
-		req: "DELETE",
-		msg: "Delete contact (with parameter /:id)..."
-	})
-})
+router.delete('/:id', auth,
+	async (req,res) => {
+		try {
+			let contact = await Contact.findById(req.params.id)
+
+			if (!contact) return res.status(404).json({ msg: 'Contact not found' })
+
+			// Make sure user owns the contact
+			if (contact.user.toString() !== req.user.id) {
+				return res.status(401).json({ msg: 'Not authorized' })
+			}
+
+			await Contact.findByIdAndRemove(req.params.id)
+
+			res.json({ msg: 'Contact removed' })
+		} catch (err) {
+			console.error(err.message)
+			res.status(500).send('Server Error')
+		}
+		// res.json({
+		// 	file: "contacts.js",
+		// 	routes: 4,
+		// 	req: "DELETE",
+		// 	msg: "Delete contact (with parameter /:id)..."
+		// })
+	}
+)
 
 module.exports = router
