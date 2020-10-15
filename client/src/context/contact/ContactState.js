a5b9f9a78079e469849cda4a34a75f3143f966b3
Ivo <ivokalendar@icloud.com>
2020-10-15 13:06:10 +0200

    Completed the filltered form

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -8,7 +8,7 @@ import {
 	SET_CURRENT,
 	CLEAR_CURRENT,
 	UPDATE_CONTACT,
-	FILTER_CONTACT,
+	FILTER_CONTACTS,
 	CLEAR_FILTER
 } from '../types'
 
@@ -44,7 +44,8 @@ const ContactState = props => {
 				type: 'professional'
 			}
 		],
-		current: null
+		current: null,
+		filtered: null
 	}
 
 	const [state, dispatch] = useReducer(contactReducer, initialState)
@@ -76,19 +77,28 @@ const ContactState = props => {
 	}
 
 	//	Filter Contacts
+	const filterContacts = text => {
+		dispatch({ type: FILTER_CONTACTS, payload: text })
+	}
 
 	// 	Clear Filter
+	const clearFilter = () => {
+		dispatch({ type: CLEAR_FILTER })
+	}
 
 	return (
 		<ContactContext.Provider 
 			value={{ 
 				contacts: state.contacts,
 				current: state.current,
+				filtered: state.filtered,
 				addContact,
 				deleteContact,
 				setCurrent,
 				clearCurrent,
-				updateContact
+				updateContact,
+				filterContacts,
+				clearFilter
 			}}
 		>
 			{ props.children }
