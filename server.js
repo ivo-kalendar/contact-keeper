1bcb714e9bd793d0052659f04e2ba6563f68b9ef
Ivo <ivokalendar@icloud.com>
2020-10-28 11:40:23 +0100

    removed brackets...

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use('/api/contacts', require('./routes/contacts'))
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use('*', (req,res) => {
+	app.use('*', (req,res) => 
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	})
+	)
 }
 
 const PORT = process.env.PORT || 7788
